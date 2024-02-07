6a0a9b04976c5ba848e19cd2555f708bbb044856
nboufford <ncboufford@gmail.com>
2024-02-07 13:41:55 +1300

    add preprocessing scripts

---
+++ summarization/format.py
@@ -0,0 +1,120 @@
+import json
+import sys
+
+def get_activity_string(activity_obj):
+    return "process with pid:" + activity_obj['id']
+
+def get_args(entity_obj):
+    print(entity_obj['annotations']['args_count'])
+    args = 'with arguments: "'
+    for i in range(1, int(entity_obj['annotations']['args_count'])):
+        if (i == 1):
+            args = args + entity_obj['annotations']['arg['+str(i)+']']
+        else:
+            args = args + ' ' + entity_obj['annotations']['arg['+str(i)+']']
+    return args + '"'
+
+def get_entity_string(entity_obj):
+    annotations = entity_obj['annotations']
+    if 'path' in annotations:
+        return "file:" + entity_obj['annotations']['path']
+    elif 'host_name' in annotations:
+        return "ip:" + entity_obj['annotations']['host_name']
+    elif 'host' in annotations:
+        return "address:" + entity_obj['annotations']['host']
+    elif 'command' in annotations:
+        return '"' + entity_obj['annotations']['command'] + '" ' + get_args(entity_obj)
+    else:
+        return ""
+    # if annotations['id'] == "file":
+    #     return "file:" + entity_obj['annotations']['path']
+    # if annotations['id'] == "socket":
+    #     return "socket:" + entity_obj['annotations']['host']
+    # else: 
+    #     return "activiy" + entity_obj['id']
+
+def get_edge_string(edge_obj):
+    annotations = edge_obj['annotations']
+    if annotations['operation'] == "read":
+        return "read from"
+    elif annotations['operation'] == "write":
+        return "wrote to"
+    elif annotations['operation'] == "execute":
+        return "executed"
+    elif annotations['operation'] == "mmap":
+        return "loaded"
+    elif annotations['operation'] == "socket_connect":
+        return "connected to"
+    elif annotations['operation'] == "socket_rcv_skb":
+        return "received data from"
+    elif annotations['operation'] == "executed_command":
+        return "ran command"
+    else:
+        return ""
+    
+def get_date(edge_obj):
+    annotations = edge_obj['annotations']
+    return annotations['datetime'] + " "
+
+def write_libs(edge_obj, entity_obj, libs_list):
+    if (edge_obj['annotations']['operation'] == "mmap"):
+        libs_list.write(entity_obj['annotations']['path'] + '\n')
+    else: 
+        libs_list.write('\n')
+
+def prov_format_gpt(prov_file_name):
+    try:
+        f_prov = open(prov_file_name, "r")        
+    except FileNotFoundError:
+        msg = prov_file_name + " does not exist."
+        print(msg)
+        return
+
+    str = prov_file_name.split(".", 1)[0]
+    new_log_name = str + "_gpt.log"
+    print("opening file " + new_log_name)
+
+    new_log = open(new_log_name, "a")
+
+    libs_list = open("libraries_list.txt", "a")
+
+    prov_lines = f_prov.readlines()
+
+    i = 0
+    while i < len(prov_lines):
+    #for i in range(len(prov_lines)):
+        entity_line = prov_lines[i]
+        activity_line = prov_lines[i+1]
+        edge_line = prov_lines[i+2]
+
+        entity_obj = json.loads(entity_line)
+        activity_obj = json.loads(activity_line)
+        edge_obj = json.loads(edge_line)
+        print(entity_obj)
+        print(activity_obj)
+        print(edge_obj)
+
+        date = get_date(edge_obj)
+        activity_str = get_activity_string(activity_obj)
+        edge_str = get_edge_string(edge_obj)
+        entity_str = get_entity_string(entity_obj)
+
+        new_line = date + activity_str + " " + edge_str + " " + entity_str + '\n'
+        print(new_line)
+        new_log.write(new_line)
+
+        write_libs(edge_obj, entity_obj, libs_list)
+
+        i = i + 3
+
+    return
+
+def main():
+    if len(sys.argv) > 1:
+        prov_file_name = sys.argv[1]
+        prov_format_gpt(prov_file_name)
+    else:
+        print("please provide provenance log")
+
+if __name__ == "__main__":
+    main()
---
+++ summarization/summarize.py
@@ -0,0 +1,131 @@
+import json
+import sys
+
+def remove_map(obj1, obj2, entity_obj):
+    if (obj1['annotations']['operation'] == "mmap"):
+        if (entity_obj['annotations']['path'][-12:] == 'lib-dynload/'):
+           return 1
+        if ('lib64/' in entity_obj['annotations']['path']):
+           return 1
+        if ('[eventpoll]' in entity_obj['annotations']['path']):
+           return 1
+    
+    return 0
+
+# this does not work
+# making up for the way thoth captures file paths. Once the file paths can be larger (and do not get truncated), we can remove this
+def fix_truncated_map(edge_line, entity_line):
+    new_entity_line = entity_line
+    if ("mmap" in edge_line):
+        if ('.' in entity_line):
+            new_entity_line = entity_line.replace('."', '.so"')
+        if ('.s"' in entity_line):
+            new_entity_line = entity_line.replace('.s"', '.so"')
+        if ('linux-gn"' in entity_line):
+            new_entity_line = entity_line.replace('linux-gn"', 'linux-gnu.so"')
+        if ('linux-g"' in entity_line):
+            new_entity_line = entity_line.replace('linux-g"', 'linux-gnu.so"')
+    return new_entity_line
+
+
+def compare_obj(obj1, obj2):
+    # print(obj1)
+    # print(obj2)
+    if obj1['to'] != obj2['to']:
+        return 0
+    if obj1['from'] != obj2['from']:
+        return 0
+    if obj1['type'] != obj2['type']:
+        return 0
+    if obj1['annotations']['operation'] != obj2['annotations']['operation']:
+        return 0
+    # lastly compare operation in the annotations, but lets see if that ever arises (it does)
+    return 1
+
+# read three lines from a file
+# prov logs should be written such that there are three lines per entry (Entity, Activty and Edge)
+# ... this might change when we add the network stuff... but probably it will still be the case that every third entry is an edge
+def read_entry(file):
+    entity = file.readline()
+    activity = file.readline()
+    edge = file.readline()
+    return edge
+
+
+# remove duplicate edges in prov log
+# each line in the file should be a json object (as per SPADE format)
+# an edge is a duplicate if it has the same to, from, type and operation as the previous line 
+def prov_summarize_edges(prov_file_name):
+    try:
+        f_prov = open(prov_file_name, "r")        
+    except FileNotFoundError:
+        msg = prov_file_name + " does not exist."
+        print(msg)
+        return
+
+    str = prov_file_name.split(".", 1)[0]
+    new_log_name = str + "_summarized.json"
+    print("opening file " + new_log_name)
+
+    new_log = open(new_log_name, "a")
+
+    prev_edge = {}
+    curr_edge = {}
+
+    # read first line
+    line = f_prov.readline()
+
+    # while read entry (3 lines)
+        # compare edge to prev edge
+        # if same then do not record
+        # if different then record all three lines
+
+    # read until end of file
+    while line:
+        print(line)
+        
+        entity_line = line
+        activity_line = f_prov.readline()
+        edge_line = f_prov.readline()
+
+        entity_line = fix_truncated_map(edge_line, entity_line)
+
+        curr_edge = json.loads(edge_line)
+        curr_entity = json.loads(entity_line)
+
+     #   new_entity = fix_truncated_map(curr_edge, curr_entity)
+
+     #   new_entity_line = json.dumps(new_entity)
+
+       # curr_obj = json.loads(line)
+        if prev_edge == {}:
+            new_log.write(entity_line)
+            new_log.write(activity_line)
+            new_log.write(edge_line)
+            # still want to add it to the log.. making base case explicit for now
+
+        elif compare_obj(prev_edge, curr_edge) == 1:
+    #        # do not add it to the new log, it is a duplicate
+            print("not adding line")
+
+        elif remove_map(curr_edge, curr_edge, curr_entity) == 1:
+            print("not adding")
+
+        else:
+            new_log.write(entity_line)
+            new_log.write(activity_line)
+            new_log.write(edge_line)
+
+        prev_edge = curr_edge
+        line = f_prov.readline()
+
+
+def main():
+    if len(sys.argv) > 1:
+        prov_file_name = sys.argv[1]
+        prov_summarize_edges(prov_file_name)
+    else:
+        print("please provide provenance log")
+
+if __name__ == "__main__":
+    main()
